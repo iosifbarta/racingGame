662c34c48eb0d2a4b8e1fc03c0088a4cf1be68c6
Iosif.Barta <iosif.barta@gmail.com>
2020-03-18 22:04:46 +0200

    studied classes variable, primitive, data types

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,36 @@
+package org.fasttrackit;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println();
+        System.out.println( "Welcome to the racing game ");
+        System.out.println();
+        Car carReference = new Car();
+        carReference.name = "Audi";
+        carReference.colour = "red";
+        carReference.maxSpeed = 260;
+        carReference.mileage = 6.2;
+
+        //concatanation
+        System.out.println("Properties of " + carReference.name);
+        System.out.println("Colour of car: " + carReference.colour);
+        System.out.println("Maximum speed:  " + carReference.maxSpeed + " km/h");
+        System.out.println("Mileage: " + carReference.mileage + " l/100 km");
+        System.out.println("Fuel level: " + carReference.fuelLevel);
+        System.out.println("Racing Number: " + carReference.racingNumber);
+        System.out.println("Damaged: " + carReference.damaged);
+        System.out.println();
+
+        Car car2 = new Car();
+        car2.name = "Lamboughini";
+        car2.damaged = true;
+        car2.colour = "Blue";
+
+        System.out.println("Proprieties of: " + car2.name);
+        System.out.println("Colour of car: " + car2.colour);
+        System.out.println("Damaged: " + car2.damaged);
+
+    }
+}
---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,14 @@
+package org.fasttrackit;
+
+public class Car {
+
+    //instance variables
+    int racingNumber;
+    String name;
+    int maxSpeed;
+    double mileage;
+    String colour;
+    double fuelLevel;
+    boolean damaged;
+
+}
