0185cdbef039d426952f96978285e478f92218e5
Iosif.Barta <iosif.barta@gmail.com>
2020-03-20 19:09:03 +0200

    added Game class

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -2,10 +2,12 @@ package org.fasttrackit;
 
 import java.util.Scanner;
 
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+
+    public static void main( String[] args ) {
+        Game game = new Game();
+        game.start();
+
         System.out.println("Please enter vehicle name: ");
         Scanner scanner = new Scanner(System.in);
         String vehicleName = scanner.nextLine();
